b91bc5b52bdec90470b57a3354a53fb093f03ed3
lan359969-blip <lan359969@gmail.com>
2026-01-17 18:19:04 +0800

    更新 storage.js

--- utils/storage.js
+++ utils/storage.js
@@ -1,1 +1,12 @@
-
+export const Storage = {
+  set(key, value) {
+    localStorage.setItem(key, JSON.stringify(value))
+  },
+  get(key, defaultValue = null) {
+    const v = localStorage.getItem(key)
+    return v ? JSON.parse(v) : defaultValue
+  },
+  remove(key) {
+    localStorage.removeItem(key)
+  }
+}
