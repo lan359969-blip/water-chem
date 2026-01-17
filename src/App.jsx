c3f4cce31d9c64a935d6226c007cb1ed17ea8efc
lan359969-blip <lan359969@gmail.com>
2026-01-18 00:04:17 +0800

    更新 App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import DosingCalculation from './DosingCalculation.jsx'
+import DosingPage from './pages/dosing/DosingPage'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dosing-calculation" element={<DosingCalculation />} />
+        <Route path="/dosing-v2" element={<DosingPage />} />
       </Routes>
     </BrowserRouter>
   )
