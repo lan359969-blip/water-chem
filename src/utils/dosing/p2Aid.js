8f4b052f256139af52b8c34827781204219e143b
lan359969-blip <lan359969@gmail.com>
2026-01-17 23:55:06 +0800

    更新 p2Aid.js

--- src/utils/dosing/p2Aid.js
+++ src/utils/dosing/p2Aid.js
@@ -8,25 +8,9 @@ export function calcP2Aid(H_raw, C_raw) {
     return '请输入有效数值'
   }
 
-  const V_drug = P2.AID.TANK_L * P2.AID.TANK_W * H
-  const M_drug = V_drug * P2.AID.DENSITY
+  const V = P2.AID.TANK_L * P2.AID.TANK_W * H
+  const M = V * P2.AID.DENSITY
+  const Vt = (M / (C / 100)) / 1000
 
-  const M_total = M_drug / (C / 100)
-  const V_total = M_total / 1000
-
-  const V_add = V_total - V_drug
-  const H_add = V_add / P2.AID.POOL_AREA
-
-  let r = ''
-  r += `计量桶原液体积：${V_drug.toFixed(2)} m³\n`
-  r += `计量桶原液质量：${M_drug.toFixed(2)} kg\n\n`
-  r += `目标所需总体积：${V_total.toFixed(2)} m³\n`
-  r += `溶液池需补水体积：${V_add.toFixed(2)} m³\n`
-  r += `折算补水高度：${H_add.toFixed(2)} m\n\n`
-
-  r += H_add > P2.AID.POOL_MAX_H
-    ? `⚠ 溶液池最终液位 ${H_add.toFixed(2)} m ＞ ${P2.AID.POOL_MAX_H} m，发生溢流`
-    : `✓ 溶液池液位 ${H_add.toFixed(2)} m，安全`
-
-  return r
+  return `需补水 ${(Vt - V).toFixed(2)} m³`
 }
