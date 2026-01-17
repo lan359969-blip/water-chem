3b4868220efe302bdeff005e0059cf96461cd837
lan359969-blip <lan359969@gmail.com>
2026-01-17 23:48:17 +0800

    删除 index.js

--- src/pages/dosing/index.js
+++
@@ -1,6 +0,0 @@
-import { calcP1Coag } from './calc/p1Coag'
-import { calcP1Aid } from './calc/p1Aid'
-import { calcP2Coag } from './calc/p2Coag'
-import { calcP2Aid } from './calc/p2Aid'
-
-setP1CoagResult(calcP1Coag(p1CoagM, p1CoagC))
