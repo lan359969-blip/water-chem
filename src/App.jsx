311f0857c263f4dc2ca184eb9b7669090eaf844e
lan359969-blip <lan359969@gmail.com>
2026-01-18 00:08:37 +0800

    更新 App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './Home'
-import DosingCalculation from './DosingCalculation.jsx'
 import DosingPage from './pages/dosing/DosingPage'
 
 function App() {
   return (
-    <BrowserRouter basename="/water-chem">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dosing-calculation" element={<DosingCalculation />} />
-        <Route path="/dosing-v2" element={<DosingPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/dosing" element={<DosingPage />} />
+    </Routes>
   )
 }
 
