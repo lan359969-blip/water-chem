7ccc808ea1320f248feb15b4be141a06f1f468f1
lan359969-blip <lan359969@gmail.com>
2026-01-17 16:36:56 +0800

    更新 vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/water-chem/',
   plugins: [react()],
   server: {
-  base: '/water-chem/',
     port: 3000,
     open: true
   }
