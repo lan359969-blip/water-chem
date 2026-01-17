098a8dee63ba6b1e7ff205cd3ee75698e2a0a223
lan359969-blip <lan359969@gmail.com>
2026-01-17 17:28:27 +0800

    更新 App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home'
-import DosingCalculation from './DosingCalculation'
+import DosingCalculation from './DosingCalculation.jsx'
 
 function App() {
   return (
