ac7db03f4ee9e572878c1519d0a226a59dbac80a
lan359969-blip <lan359969@gmail.com>
2026-01-17 18:18:00 +0800

    更新 InputField.jsx

--- components/InputField.jsx
+++ components/InputField.jsx
@@ -1,1 +1,13 @@
-keeep
+export default function InputField({ label, value, onChange }) {
+  return (
+    <div className="input-field">
+      <label className="input-label">{label}</label>
+      <input
+        type="number"
+        value={value}
+        onChange={onChange}
+        className="input"
+      />
+    </div>
+  )
+}
