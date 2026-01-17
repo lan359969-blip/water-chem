75f81e152a607fc4b477233a3605ff1c4a23b58f
lan359969-blip <lan359969@gmail.com>
2026-01-18 00:44:25 +0800

    更新 App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './Home'
-import DosingPage from './pages/dosing/DosingPage'
+import Dosing from './Dosing'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/dosing" element={<DosingPage />} />
+      <Route path="/dosing" element={<Dosing />} />
     </Routes>
   )
 }
