ab96b591a253357930cfbd677ecca69b7cd22dfe
lan359969-blip <lan359969@gmail.com>
2026-01-17 23:39:50 +0800

    更新 index.js

--- src/pages/dosing/index.js
+++ src/pages/dosing/index.js
@@ -1,1 +1,6 @@
+import { calcP1Coag } from './calc/p1Coag'
+import { calcP1Aid } from './calc/p1Aid'
+import { calcP2Coag } from './calc/p2Coag'
+import { calcP2Aid } from './calc/p2Aid'
 
+setP1CoagResult(calcP1Coag(p1CoagM, p1CoagC))
