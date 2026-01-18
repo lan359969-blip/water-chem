48bf0a74635c7029d6d6ef09114eb5608a33e08f
lan359969-blip <lan359969@gmail.com>
2026-01-18 14:51:46 +0800

    创建 dosing.config.js

---
+++ src/config/dosing.config.js
@@ -0,0 +1,31 @@
+export const DOSING = {
+  P1: {
+    COAG: {
+      POOL_DIAMETER: 1.2,   // m
+      POOL_AREA: 1.13,      // m²
+      DENSITY: 1.08,        // kg/L
+      MAX_H: 2.8            // m
+    },
+    AID: {
+      POOL_DIAMETER: 1.0,
+      POOL_AREA: 0.79,
+      DENSITY: 1.05,
+      MAX_H: 2.5
+    }
+  },
+
+  P2: {
+    COAG: {
+      POOL_DIAMETER: 1.5,
+      POOL_AREA: 1.77,
+      DENSITY: 1.1,
+      MAX_H: 3.0
+    },
+    AID: {
+      POOL_DIAMETER: 1.2,
+      POOL_AREA: 1.13,
+      DENSITY: 1.06,
+      MAX_H: 2.6
+    }
+  }
+}
