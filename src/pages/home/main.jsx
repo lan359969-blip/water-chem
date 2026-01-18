70b5b2f5a161d09707bc91f292a6e4f839d9c452
lan359969-blip <lan359969@gmail.com>
2026-01-18 17:15:04 +0800

    更新 main.jsx

--- src/pages/home/main.jsx
+++ src/pages/home/main.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
+import DosingPage from '../dosing/DosingPage'
 
 function Home() {
+  const [page, setPage] = useState('home')
+
+  if (page === 'dosing') {
+    return <DosingPage />
+  }
+
   return (
     <div style={{ padding: 20 }}>
       <h1>水厂加药与配药系统</h1>
-
-      <a href="./src/pages/dosing/index.html">
+      <button onClick={() => setPage('dosing')}>
         进入配药计算模块
-      </a>
+      </button>
     </div>
   )
 }
