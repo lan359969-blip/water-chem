fb71db252df69b61db2dc362a516a54b7de2825a
lan359969-blip <lan359969@gmail.com>
2026-01-18 10:22:56 +0800

    创建 main.jsx

---
+++ src/pages/home/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>水厂加药与配药系统</h1>
+
+      <a href="../dosing.html">
+        进入配药计算模块
+      </a>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Home />
+)
