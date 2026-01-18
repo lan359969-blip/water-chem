493e7ebcfee9e0c13bdb18297cb04df5b9dcca87
lan359969-blip <lan359969@gmail.com>
2026-01-18 16:20:03 +0800

    创建 constants.js

---
+++ src/modules/dosing/shared/constants.js
@@ -0,0 +1,13 @@
+// src/modules/dosing/shared/constants.js
+
+export const PI = Math.PI
+
+// 单位换算
+export const MG_L_TO_KG_M3 = 0.001
+
+// 默认精度（页面展示用）
+export const PRECISION = {
+  mass: 2,     // kg
+  volume: 3,   // m³
+  height: 2    // m
+}
