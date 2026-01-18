1591757d2c9a48fbb75403f5a9ef3ac678bcdd9a
lan359969-blip <lan359969@gmail.com>
2026-01-18 18:10:48 +0800

    更新 vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   base: '/water-chem/',
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: {
-        home: resolve(__dirname, 'src/pages/home/index.html'),
-        dosing: resolve(__dirname, 'src/pages/dosing/index.html')
-      }
-    }
+    outDir: 'docs'
   }
 })
