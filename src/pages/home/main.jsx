910014fb195aa5728cf9386545881efd2fad4f0e
lan359969-blip <lan359969@gmail.com>
2026-01-18 13:00:19 +0800

    更新 main.jsx

--- src/pages/home/main.jsx
+++ src/pages/home/main.jsx
@@ -6,7 +6,7 @@ function Home() {
     <div style={{ padding: 20 }}>
       <h1>水厂加药与配药系统</h1>
 
-      <a href="./dosing/index.html">
+      <a href="./src/pages/dosing/index.html">
         进入配药计算模块
       </a>
     </div>
