be3f7d00c7cb6fca079558881c2f580caff91399
lan359969-blip <lan359969@gmail.com>
2026-01-18 17:17:53 +0800

    删除 main.jsx

--- src/pages/dosing/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import DosingPage from './DosingPage'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <DosingPage />
-)
