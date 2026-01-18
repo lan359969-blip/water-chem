4d4b728fe3d9150cd4b9a0d40018abb7df52a588
lan359969-blip <lan359969@gmail.com>
2026-01-18 15:12:58 +0800

    更新 formula.js

--- src/modules/dosing/shared/formula.js
+++ src/modules/dosing/shared/formula.js
@@ -1,1 +1,13 @@
-keep
+import { PI } from './unit'
+
+export function cylinderVolume(radius, height) {
+  return PI * radius ** 2 * height
+}
+
+export function mass(volume, density) {
+  return volume * density
+}
+
+export function addWaterHeight(deltaVolume, poolArea) {
+  return deltaVolume / poolArea
+}
