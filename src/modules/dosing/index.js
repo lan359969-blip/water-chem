af0f1d76f6b7c399175575550917ece319d833b5
lan359969-blip <lan359969@gmail.com>
2026-01-19 10:22:50 +0800

    更新 index.js

--- src/modules/dosing/index.js
+++ src/modules/dosing/index.js
@@ -1,2 +1,4 @@
-export { calcP1Coag, calcP1Aid } from './p1'
-export { calcP2Coag, calcP2Aid } from './p2'
+export { calcP1Coag } from './p1/coag'
+export { calcP1Aid }  from './p1/aid'
+export { calcP2Coag } from './p2/coag'
+export { calcP2Aid }  from './p2/aid'
